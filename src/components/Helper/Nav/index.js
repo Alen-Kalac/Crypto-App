dc2251dfc76092a7967dc1572a44d435d51b3d32
demirsubasic2001 <subasicd2000@gmail.com>
2023-07-22 19:31:28 +0200

    added-navbar-and-footer

--- src/components/Helper/Nav/index.js
+++ src/components/Helper/Nav/index.js
@@ -1,11 +1,30 @@
+import { NavLink } from 'react-router-dom';
 import './index.scss';
+import {AiOutlineHeart} from 'react-icons/ai'
+import {FaUserCircle} from 'react-icons/fa'
 
-function Header() {
+function Navbar() {
   return (
     <>
-    Header
+    <div className='navbar'>
+      <div className='logo'>
+        <NavLink to='/'> <img src="https://cdn.icon-icons.com/icons2/2389/PNG/512/bitcoin_logo_icon_145452.png" alt="" />CRYPTO-APP</NavLink>
+        
+      </div>
+      <div className='nav-items'>
+        
+      <span><NavLink to='/'>Home</NavLink></span>
+      <span><NavLink to='/coins'>Coins</NavLink></span>
+      <span><NavLink to='/exchanges'>Exchanges</NavLink></span>
+      <span><NavLink to='/about-us'>About Us</NavLink></span>
+      <span className='heart'><NavLink to='/favorites'><AiOutlineHeart /></NavLink></span>
+      </div>
+      <div className='profile'>
+        <span><NavLink to='/my-profile'><FaUserCircle /></NavLink></span>
+      </div>
+    </div>
     </>
   );
 }
 
-export default Header;
+export default Navbar;
