6746f596140e5dab4c022f8673e54f150b4775f9
demirsubasic2001 <subasicd2000@gmail.com>
2023-07-23 17:07:21 +0200

    fetching-api-for-coins-section

--- src/components/Pages/Coins/index.js
+++ src/components/Pages/Coins/index.js
@@ -1,8 +1,43 @@
 import './index.scss';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function Coins() {
+  const [coinData, setCoinData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const options = {
+        method: 'GET',
+        url: 'https://coinranking1.p.rapidapi.com/coins',
+        params: {
+          referenceCurrencyUuid: 'yhjMzLPhuIDl',
+          timePeriod: '24h',
+          'tiers[0]': '1',
+          orderBy: 'marketCap',
+          orderDirection: 'desc',
+          limit: '12',
+          offset: '0',
+        },
+        headers: {
+          'X-RapidAPI-Key': '4fc7a0a046mshe639402022eb2b2p13e611jsn6d88e3ba564e',
+          'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+        },
+      };
+
+      try {
+        const response = await axios.request(options);
+        setCoinData(response.data?.data?.coins || []);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <>
+    
     </>
   );
 }
